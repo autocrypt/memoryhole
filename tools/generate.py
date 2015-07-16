62ee14f0cc9540d3aa3ee42499804206eaaf3e3f
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2015-07-16 16:09:23 +0200

    complete signed message with example

--- tools/generate.py
+++ tools/generate.py
@@ -11,7 +11,7 @@ import subprocess
 
 m = email.message.Message()
 
-m.add_header('Subject', 'Test')
+m.add_header('Subject', 'A silly message')
 m.add_header('Date', 'Thu, 16 Jul 2015 11:44:44 +0200')
 m.add_header('Message-ID', 'A@memoryhole.example')
 m.add_header('To', 'Julia <julia@example.org>')
@@ -49,6 +49,17 @@ XXXXXX
 -----END PGP SIGNATURE-----
 '''
 
+embedded_header = ''
+for x in ['Date', 'Subject', 'From', 'To', 'Message-ID']:
+    embedded_header += x + ': ' + m.get(x)
+
+wrapper = email.message.Message()
+wrapper.set_type('multipart/mixed')
+
+emh = email.message.Message()
+emh.set_type('text/rfc822-header')
+emh.set_payload(embedded_header)
+
 s = email.message.Message()
 s.set_type('multipart/alternative')
 s.set_boundary('yyyyyyyyyyyyy')
@@ -65,6 +76,7 @@ h.set_payload(html)
 
 s.set_payload([t,h])
 
+wrapper.set_payload([emh,s])
 
 sig = email.message.Message()
 sig.set_type('application/pgp-signature')
@@ -80,12 +92,17 @@ g = subprocess.Popen(['gpg2', '--batch',
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE)
 
-(sout, serr) = g.communicate(s.as_bytes())
+
+# FIXME: this is a sloppy conversion, because it would convert any thing already crlf
+# FIXME: it's also possible that this should do a conversion to string
+# form or something, instead of using raw bytes
+(sout, serr) = g.communicate(wrapper.as_bytes().replace(b'\n', b'\r\n'))
+
 sig.set_payload(sout)
 if serr is not None:
     sys.stderr.buffer.write(serr)
 
-m.set_payload([s,sig])
+m.set_payload([wrapper,sig])
 
 sys.stdout.buffer.write(m.as_bytes())
 
