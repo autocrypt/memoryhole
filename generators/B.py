5490cecb38cf87011a86c168fded695384c84cba
Kareem Khazem <karkhaz@karkhaz.com>
2015-07-20 02:40:49 +0100

    unsigned mail with embedded header

--- generators/B.py
+++ generators/B.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 description = (
-'alternative text/html message with embedded header, signed'
+'alternative text/html message with embedded header, unsigned'
 )
 
 import email.message
@@ -19,9 +19,7 @@ m.add_header('Date', 'Thu, 16 Jul 2015 11:44:44 +0200')
 m.add_header('Message-ID', 'A@memoryhole.example')
 m.add_header('To', 'Julia <julia@example.org>')
 m.add_header('From', 'Winston <winston@example.net>')
-m.set_type('multipart/signed')
-m.set_param('micalg', 'pgp-sha256')
-m.set_param('protocol', 'application/pgp-signature')
+m.set_type('multipart/mixed')
 m.set_boundary('xxxxxxxxxxxx')
 
 txt = '''This is a test
@@ -44,14 +42,9 @@ html = '''<html>
 </body>
 </html>
 '''
-
 embedded_header = ''
 for x in ['Date', 'Subject', 'From', 'To', 'Message-ID']:
     embedded_header += x + ': ' + m.get(x) + '\n'
-
-wrapper = email.message.Message()
-wrapper.set_type('multipart/mixed')
-wrapper.set_boundary('zzzzzzzzzzzz')
 
 emh = email.message.Message()
 emh.set_type('text/rfc822-header')
@@ -74,33 +67,7 @@ h.set_payload(html)
 
 s.set_payload([t,h])
 
-wrapper.set_payload([emh,s])
-
-sig = email.message.Message()
-sig.set_type('application/pgp-signature')
-
-g = subprocess.Popen(['gpg2', '--batch',
-                      '--homedir=../corpus/OpenPGP/GNUPGHOME',
-                      '--pinentry-mode=loopback',
-                      '--passphrase=_winston_',
-                      '--armor', '--detach-sign',
-                      '--digest-algo=sha256',
-                      '-u', 'winston@example.net'],
-                     stdin=subprocess.PIPE,
-                     stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE)
-
-
-# FIXME: this is a sloppy conversion, because it would convert any thing already crlf
-# FIXME: it's also possible that this should do a conversion to string
-# form or something, instead of using raw bytes
-(sout, serr) = g.communicate(wrapper.as_bytes().replace(b'\n', b'\r\n'))
-
-sig.set_payload(sout)
-if serr is not None:
-    sys.stderr.buffer.write(serr)
-
-m.set_payload([wrapper,sig])
+m.set_payload([emh,s])
 
 sys.stdout.buffer.write(m.as_bytes())
 
