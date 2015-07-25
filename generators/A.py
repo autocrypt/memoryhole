f363ec35e753f2b0e08e8867ae6fa34e16a4d982
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2015-07-25 11:57:59 +0100

    move most of generation code to a class

--- generators/A.py
+++ generators/A.py
@@ -1,101 +1,15 @@
 #!/usr/bin/python3
 
-description = (
-'alternative text/html message with embedded header, signed'
-)
+from generator import Generator, gen_multipart_alternative
 
-# ====== Do not change this line =====================================
-#
-# common/common.py gets put here. m is a message; Subject and
-# Message-ID are automatically generated, but you can customize all
-# other aspects of the message.
-
+m = Generator('alternative text/html message with embedded header, signed', __file__)
 
 m.add_header('Date', 'Thu, 16 Jul 2015 11:44:44 +0200')
 m.add_header('To', 'Julia <julia@example.org>')
 m.add_header('From', 'Winston <winston@example.net>')
-m.set_type('multipart/signed')
-m.set_param('micalg', 'pgp-sha256')
-m.set_param('protocol', 'application/pgp-signature')
-m.set_boundary('xxxxxxxxxxxx')
 
-txt = '''This is a test
-message on multiple lines
+s = gen_multipart_alternative()
+b = m.wrap_with_header(s)
+m.sign(b)
 
-with a silly bit more.
--- 
-and a .sig here.
-'''
-
-html = '''<html>
-<head>
-<title>titles are usually unrendered</title>
-</head>
-<body>
-<p>This is a test<br/>message on multiple lines</p>
-<p>with a silly bit more.</p>
-<hr/>
-<p>and a .sig here.</p>
-</body>
-</html>
-'''
-
-embedded_header = ''
-for x in ['Date', 'Subject', 'From', 'To', 'Message-ID']:
-    embedded_header += x + ': ' + m.get(x) + '\n'
-
-wrapper = email.message.Message()
-wrapper.set_type('multipart/mixed')
-wrapper.set_boundary('zzzzzzzzzzzz')
-
-emh = email.message.Message()
-emh.set_type('text/rfc822-header')
-emh.add_header('Content-Disposition', 'attachment')
-emh.set_payload(embedded_header)
-
-s = email.message.Message()
-s.set_type('multipart/alternative')
-s.set_boundary('yyyyyyyyyyyyy')
-
-
-
-t = email.message.Message()
-t.set_type('text/plain')
-t.set_payload(txt)
-
-h = email.message.Message()
-h.set_type('text/html')
-h.set_payload(html)
-
-s.set_payload([t,h])
-
-wrapper.set_payload([emh,s])
-
-sig = email.message.Message()
-sig.set_type('application/pgp-signature')
-
-g = subprocess.Popen(['gpg2', '--batch',
-                      '--homedir=corpus/OpenPGP/GNUPGHOME',
-                      '--pinentry-mode=loopback',
-                      '--passphrase=_winston_',
-                      '--armor', '--detach-sign',
-                      '--digest-algo=sha256',
-                      '-u', 'winston@example.net'],
-                     stdin=subprocess.PIPE,
-                     stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE)
-
-
-# FIXME: this is a sloppy conversion, because it would convert any thing already crlf
-# FIXME: it's also possible that this should do a conversion to string
-# form or something, instead of using raw bytes
-(sout, serr) = g.communicate(wrapper.as_bytes().replace(b'\n', b'\r\n'))
-
-sig.set_payload(sout)
-if serr is not None:
-    sys.stderr.buffer.write(serr)
-
-m.set_payload([wrapper,sig])
-
-sys.stdout.buffer.write(m.as_bytes())
-
+m.main()
---
+++ generators/generator.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*
+
+import email.message
+import sys
+import os
+import subprocess
+
+data = {
+    'txt': '''This is a test
+message on multiple lines
+
+with a silly bit more.
+-- 
+and a .sig here.
+''',
+    'html': '''<html>
+<head>
+<title>titles are usually unrendered</title>
+</head>
+<body>
+<p>This is a test<br/>message on multiple lines</p>
+<p>with a silly bit more.</p>
+<hr/>
+<p>and a .sig here.</p>
+</body>
+</html>
+''',
+    'bgen': ord('a')
+}
+
+    
+def gen_boundary():
+    r = chr(data['bgen']) * 12
+    data['bgen'] += 1
+    return r
+
+def gen_text_plain():
+    t = email.message.Message()
+    t.set_type('text/plain')
+    t.set_payload(data['txt'])
+    return t
+    
+def gen_text_html():
+    h = email.message.Message()
+    h.set_type('text/html')
+    h.set_payload(data['html'])
+    return h
+
+def gen_multipart_alternative():
+    s = email.message.Message()
+    s.set_type('multipart/alternative')
+    s.set_boundary(gen_boundary())
+    s.set_payload([gen_text_plain(),gen_text_html()])
+    return s
+
+
+# adapted from notmuch:devel/printmimestructure 
+def render_mime_structure(z, prefix='└', stream=sys.stdout):
+    '''z should be an email.message.Message object'''
+    fname = '' if z.get_filename() is None else ' [' + z.get_filename() + ']'
+    cset = '' if z.get_charset() is None else ' (' + z.get_charset() + ')'
+    disp = z.get_params(None, header='Content-Disposition')
+    if (disp is None):
+        disposition = ''
+    else:
+        disposition = ''
+        for d in disp:
+            if d[0] in [ 'attachment', 'inline' ]:
+                disposition = ' ' + d[0]
+    if (z.is_multipart()):
+        print(prefix + '┬╴' + z.get_content_type() + cset +
+                disposition + fname, z.as_string().__len__().__str__()
+                + ' bytes', file=stream)
+        if prefix.endswith('└'):
+            prefix = prefix.rpartition('└')[0] + ' '
+        if prefix.endswith('├'):
+            prefix = prefix.rpartition('├')[0] + '│'
+        parts = z.get_payload()
+        i = 0
+        while (i < parts.__len__()-1):
+            render_mime_structure(parts[i], prefix + '├', stream=stream)
+            i += 1
+        render_mime_structure(parts[i], prefix + '└', stream=stream)
+        # FIXME: show epilogue?
+    else:
+        print(prefix + '─╴'+ z.get_content_type() + cset + disposition
+                + fname, z.get_payload().__len__().__str__(), 'bytes', file=stream)
+
+
+class Generator(email.message.Message):
+    def __init__(self, description, messagename):
+        email.message.Message.__init__(self)
+        self.messagename = os.path.splitext(os.path.basename(messagename))[0]
+        self.description = description
+        self.add_header('Subject', description)
+        self.add_header('Message-ID',  self.messagename + '@memoryhole.example')
+
+    def __str__(self):
+        return 'E-mail generator (' + self.description + ')'
+
+
+    def build_embedded_header(self):
+        r = ''
+        for x in ['Date', 'Subject', 'From', 'To', 'Message-ID']:
+            if self.get(x):
+                r += x + ': ' + self.get(x) + '\n'
+        return r
+    def wrap_with_header(self, body):
+        emh = email.message.Message()
+        emh.set_type('text/rfc822-header')
+        emh.add_header('Content-Disposition', 'attachment')
+        emh.set_payload(self.build_embedded_header())
+        
+        wrapper = email.message.Message()
+        wrapper.set_type('multipart/mixed')
+        wrapper.set_boundary(gen_boundary())
+
+        wrapper.set_payload([emh,body])
+        return wrapper
+
+    def get_password(self):
+        '''This returns the expected password based on the From: address'''
+        return re.sub(r'.*?([^@<]*)@.*', r'_\1_', self.get('From'))
+    def sign(self,body):
+        g = subprocess.Popen(['gpg2', '--batch',
+                              '--homedir=corpus/OpenPGP/GNUPGHOME',
+                              '--pinentry-mode=loopback',
+                              '--passphrase=_winston_',
+                              '--armor', '--detach-sign',
+                              '--digest-algo=sha256',
+                              '-u', self.get('From')],
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        
+
+        # FIXME: this is a sloppy conversion, because it would convert any thing already crlf
+        # FIXME: it's also possible that this should do a conversion to string
+        # form or something, instead of using raw bytes
+        (sout, serr) = g.communicate(body.as_bytes().replace(b'\n', b'\r\n'))
+        if serr is not None:
+            sys.stderr.buffer.write(serr)
+
+        sig = email.message.Message()
+        sig.set_type('application/pgp-signature')
+        sig.set_payload(sout)
+
+        self.set_type('multipart/signed')
+        self.set_param('micalg', 'pgp-sha256')
+        self.set_param('protocol', 'application/pgp-signature')
+        self.set_boundary(gen_boundary())
+
+        self.set_payload([body,sig])
+
+        
+    def main(self):
+        if(len(sys.argv) > 1):
+            if sys.argv[1] == 'description':
+                print(self.description)
+                print('')
+                render_mime_structure(self, stream=sys.stdout)
+            else:
+                print('only takes one subcommand: "description"', file=sys.sterr)
+                exit(1)
+        else:
+            sys.stdout.buffer.write(self.as_bytes())
+
